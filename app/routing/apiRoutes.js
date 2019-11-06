43b46d968c0653b2161c42384543c7e93d2b7bca
Joel Margosian <margosij@gmail.com>
2019-11-05 22:03:47 -0500

    getting closer to fixing the friends extension

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -3,27 +3,44 @@
 //    * A GET route with the url `/api/friends`. This will be used to display a JSON of all possible friends.
 //    * A POST routes `/api/friends`. This will be used to handle incoming survey results. This route will also be used to handle the compatibility logic.
 
-var express = require("express");
-var path = require("path");
 
-var app = express();
-var PORT = 8080;
+var getFriends = require("../data/friends")
 
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+module.exports = function(app){
+    app.get("/api/friends", function(req, res) {
+            res.json(getFriends)
+          });
 
-app.get("/api/friends", function(req, res) {
-    console.log("Test")
-    // return res.json(path.join(__dirname, "../data/friends.js"));
-  });
+    app.post("/api/friends", function(req,res){
+        getFriends.push(req.body)
+        res.json(true)
+    })
+        
+}
 
-app.post("/api/friends", function(req, res) {
-    res.sendFile(path.join(__dirname, "../data/friends.js"));
-    var newFriend = req.body
-    friends.push(newFriend)
-    res.json(newFriend)
-});
 
-app.listen(PORT, function() {
-    console.log("App listening on PORT " + PORT);
-});
+
+// var express = require("express");
+// var path = require("path");
+
+// var app = express();
+// var PORT = 8080;
+
+// app.use(express.urlencoded({ extended: true }));
+// app.use(express.json());
+
+// app.get("/api/friends", function(req, res) {
+//     console.log("Test")
+//     // return res.json(path.join(__dirname, "../data/friends.js"));
+//   });
+
+// app.post("/api/friends", function(req, res) {
+//     res.sendFile(path.join(__dirname, "../data/friends.js"));
+//     var newFriend = req.body
+//     friends.push(newFriend)
+//     res.json(newFriend)
+// });
+
+// app.listen(PORT, function() {
+//     console.log("App listening on PORT " + PORT);
+// });
