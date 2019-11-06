34948a4e59b6060bc0ed9f8d9d07ef0cc8dac69d
Joel Margosian <margosij@gmail.com>
2019-11-05 23:01:15 -0500

    servers are finally working including the friends one

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -1,20 +1,13 @@
-var express = require("express");
+
 var path = require("path");
 
-var app = express();
-var PORT = 8080;
+module.exports = function(app){
 
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+    app.get("/survey", function(req, res) {
+        res.sendFile(path.join(__dirname, "../public/survey.html"));
+    });
 
-app.get("/survey", function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/survey.html"));
-  });
-
-app.get("/", function(req, res){
-    res.sendFile(path.join(__dirname, "../public/home.html"));
-});
-
-app.listen(PORT, function() {
-    console.log("App listening on PORT " + PORT);
-});
+    app.get("/", function(req, res){
+        res.sendFile(path.join(__dirname, "../public/home.html"));
+    });
+}
